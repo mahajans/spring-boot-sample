680234be46b644b71e96e2327f97a3cf5fa59264
smahajan <smahajan_12@yahoo.in>
2018-02-10 16:37:57 +0530

    Initial setup with:  - intellij  - gradle  - spring-boot minimal greeting app

---
+++ src/main/java/me/sourabh/springboot/domain/Greeting.java
@@ -0,0 +1,28 @@
+package me.sourabh.springboot.domain;
+
+public class Greeting {
+
+    private long id;
+    private String name;
+
+    public Greeting(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
